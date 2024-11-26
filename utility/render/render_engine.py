64d371f54bf62b5091e57cc19cffb61cb0635e01
ManoharanR24 <manoharanr24@gmail.com>
2024-11-26 12:57:18 +0530

    Update render_engine.py

--- utility/render/render_engine.py
+++ utility/render/render_engine.py
@@ -16,6 +16,9 @@ def download_file(url, filename):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
+            headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
             response = requests.get(url, headers=headers)
             f.write(response.content)
 
